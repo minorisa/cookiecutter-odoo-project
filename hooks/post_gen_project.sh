b18489727e0123a53ec64e9af14d8c935031eba6
Jaume Planas <jaume.planas@minorisa.net>
2018-05-09 11:50:51 +0200

    Test

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+PWD=`pwd`
+echo $PWD
 git clone git@git.desarrollo.minorisa.net:odoo/addons_pro -b 11.0 addons_pro
 virtualenv -p python3 .
 source bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 pip install -r OCA-requirements.txt
-mkdir addons_{{project_name}}
-cd addons_{{project_name}}
+mkdir addons_{{cookiecutter.project_name}}
+cd addons_{{cookiecutter.project_name}}
 git init
-git remote add git@git.desarrollo.minorisa.net:odoo/addons_{{project_name}}.git
+git remote add origin git@git.desarrollo.minorisa.net:odoo/addons_{{cookiecutter.project_name}}.git
 cd ..
 
