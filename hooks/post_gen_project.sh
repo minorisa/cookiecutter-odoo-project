4c010faec7d2c3dfe3e05ae7027b7ea9c21a0a56
Jaume Planas <jaume.planas@minorisa.net>
2018-05-09 11:56:38 +0200

    Add Python 3 venv support.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 PWD=`pwd`
-echo $PWD
+ENV=${PWD}/env
 git clone git@git.desarrollo.minorisa.net:odoo/addons_pro -b 11.0 addons_pro
-virtualenv -p python3 .
-source bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-pip install -r OCA-requirements.txt
-mkdir addons_{{cookiecutter.project_name}}
-cd addons_{{cookiecutter.project_name}}
+python3 -m venv $ENV
+$ENV/pip install --upgrade pip
+$ENV/pip install -r requirements.txt
+$ENV/pip install -r OCA-requirements.txt
+mkdir addons_{{ cookiecutter.project_name }}
+cd addons_{{ cookiecutter.project_name }}
 git init
-git remote add origin git@git.desarrollo.minorisa.net:odoo/addons_{{cookiecutter.project_name}}.git
-cd ..
+git remote add origin git@git.desarrollo.minorisa.net:odoo/addons_{{ cookiecutter.project_name }}.git
+cd $PWD
 
