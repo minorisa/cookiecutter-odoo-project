2bee0df1d7a7136937547473d3e78330c56be733
Jaume Planas <jaume.planas@minorisa.net>
2018-05-11 08:48:50 +0200

    Only install: create addons & internal gitlab access to be studied.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,14 +2,14 @@
 
 PWD=`pwd`
 ENV=${PWD}/env
-git clone git@git.desarrollo.minorisa.net:odoo/addons_pro -b 11.0 addons_pro
+# git clone git@git.desarrollo.minorisa.net:odoo/addons_pro -b 11.0 addons_pro
 python3 -m venv $ENV
 $ENV/bin/pip install --upgrade pip
 $ENV/bin/pip install -r requirements.txt
 $ENV/bin/pip install -r OCA-requirements.txt
-mkdir addons_{{ cookiecutter.project_name }}
-cd addons_{{ cookiecutter.project_name }}
-git init
-git remote add origin git@git.desarrollo.minorisa.net:odoo/addons_{{ cookiecutter.project_name }}.git
-cd $PWD
+# mkdir addons_{{ cookiecutter.project_name }}
+# cd addons_{{ cookiecutter.project_name }}
+# git init
+# git remote add origin git@git.desarrollo.minorisa.net:odoo/addons_{{ cookiecutter.project_name }}.git
+# cd $PWD
 
