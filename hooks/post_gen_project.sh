0fd56d90b413aef975f6db5d5a58c725a2721e97
Jaume Planas <jaume.planas@minorisa.net>
2018-05-14 13:22:14 +0200

    Fix post_gen_project.sh

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,11 +3,10 @@
 PWD=`pwd`
 ENV=${PWD}/env
 export LC_ALL=C
-# git clone git@git.desarrollo.minorisa.net:odoo/addons_pro -b 11.0 addons_pro
 python3 -m venv $ENV
 $ENV/bin/pip install --upgrade pip
 $ENV/bin/pip install -r requirements.txt
-$ENV/bin/pip install -r OCA-requirements.txt
+# $ENV/bin/pip install -r OCA-requirements.txt
 # mkdir addons_{{ cookiecutter.project_name }}
 # cd addons_{{ cookiecutter.project_name }}
 # git init
